b70fa79e5412f484108971fd55896b24cb74e905
Salon <rautsalon@gmail.com>
2025-01-27 21:30:26 +0545

    Initial Commit

---
+++ models/user.js
@@ -0,0 +1,13 @@
+//this is user.js file inside models folder, which is bascically a user model
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/application1');
+
+
+const userSchema = mongoose.Schema({
+    image:String,
+    email:String,
+    name:String
+})
+
+module.exports = mongoose.model('user', userSchema);
